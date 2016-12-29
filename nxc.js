4f621c7984a988d8ffabf85c4b9201e1043993db
grant <grant@iowntheinter.net>
2016-12-29 20:20:02 +0000

    support inital redirect

--- nxc.js
+++ nxc.js
@@ -5,58 +5,80 @@ var encoding = 'utf-8';
 var data;
 
 program
-	.version('0.0.1')
-	.option('-c, --connect <value>',	'connect to host:port')
-	.option('-n, --channel <value>',	'connect channel name')
-	.option('-l, --listen ', 		'listen')
-	.option('-p, --publish ', 		'publish')
-	.parse(process.argv);
+    .version('0.0.1')
+    .option('-c, --connect <value>', 'connect to host:port')
+    .option('-n, --channel <value>', 'connect channel name')
+    .option('-l, --listen ', 'listen')
+    .option('-p, --publish ', 'publish')
+    .parse(process.argv);
 
-if(process.env.CHANLIST)
-{
-  program.chanlist=process.env.CHANLIST
+if (process.env.CHANLIST) {
+    program.chanlist = process.env.CHANLIST
 }
-if(!program.connect)
-  program.connect='http://localhost:6500/eb/';
+if (!program.connect)
+    try {
+        program.connect = 'http://localhost:6500/eb/';
+    } catch (e) {
+        console.log('error?')
+        onerrorEventBus(e)
+    }
 
 
-var eb = new EventBus(program.connect);
-function send(msg){
-  if(program.publish)
-    eb.publish(program.channel,msg);
-  else
-    eb.send(program.channel,msg)
+function send(msg) {
+    if (program.publish)
+        eb.publish(program.channel, msg);
+    else
+        eb.send(program.channel, msg)
 }
-
-eb.onopen=function(){
-  if(!process.stdin.isTTY) {
-
-  process.stdin.setEncoding(encoding);
-  process.stdin.on('readable', function() {
-    var dat = process.stdin.read()
-    if( dat !== null )
-      send(dat)
-  });
-
-  process.stdin.on('end', function () {
-    if (!program.listen)
-      process.exit(0)
-  });
-}
-
-  if(program.listen){
-    eb.registerHandler(program.channel, function (e, mes) {
-        if (e) console.error(e);
-        else {
-            var m = mes.body;
-            console.log(m);
-        }
-    });
-  }
-};
 
 function onerrorEventBus(error) {
     console.error("Problem with event bus " + JSON.stringify(error))
 }
 
-eb.onerror = onerrorEventBus;
+
+var request = require('request');
+request(program.connect, function (error, response, body) {
+    if (error) {
+        console.log(body);
+        process.exit(-1)
+    }
+    if (response.statusCode < 400 && response.statusCode > 299) {
+        program.connect = response.header['Location'];
+        response.statusCode = 299
+    }
+    var eb = new EventBus(program.connect);
+
+    eb.onopen = function () {
+        if (!process.stdin.isTTY) {
+            eb.onerror = onerrorEventBus;
+
+            process.stdin.setEncoding(encoding);
+            process.stdin.on('readable', function () {
+                var dat = process.stdin.read();
+                if (dat !== null)
+                    send(dat)
+            });
+
+            process.stdin.on('end', function () {
+                if (!program.listen)
+                    process.exit(0)
+            });
+        }
+
+        if (program.listen) {
+            eb.registerHandler(program.channel, function (e, mes) {
+                if (e) console.error(e);
+                else {
+                    var m = mes.body;
+                    console.log(m);
+                }
+            });
+        }
+    };
+
+})
+
+
+
+
+
